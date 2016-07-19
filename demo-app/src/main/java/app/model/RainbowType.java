71fe30d3510ee5c835f931e3181ec0911866ee07
lev <lev.plotkin@gmail.com>
2016-07-19 12:38:33 +0300

    add demp-app

---
+++ demo-app/src/main/java/app/model/RainbowType.java
@@ -0,0 +1,13 @@
+package app.model;
+
+/**
+ * Created by lev on 7/19/16.
+ */
+public enum RainbowType {
+    Twinned,
+    FullCircle,
+    Multiple,
+    Supernumerary,
+    Monochrome,
+    Fogbow
+}
